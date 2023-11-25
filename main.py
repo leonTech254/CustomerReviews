316969bbd031ab941e1785cb8cd3ae76eaee6442
leonTech254 <martinleontech23@gmail.com>
2023-11-25 10:37:34 +0300

    --p

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from Flask import flask
+from flask import Flask,render_template,request
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
